15544556940b946fcb23c6327600e8d878cac4e3
LuigiDiS <luigi.distefano69@gmail.com>
2022-09-07 16:51:27 +0200

    Non riesco a far apparire le immagini

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>
+    <div class="container">
+        <div class="row gap-3">
+            <div class="card col-5" style="width: 18rem;" v-for="(card,i) in cardList" :key="i">
+                <img 
+                    :src="card.poster" 
+                    class="card-img-top" 
+                    alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{cardList.title}}</h5>
+                    <p class="card-text">{{card.author}}</p>
+
+                </div>
+            </div>
+        </div>
+
+
 
     </div>
 </template>
@@ -21,10 +36,17 @@ export default {
                 console.log(res.data)
                 this.cardList = res.data
             })
+            .finally(() => {
+                console.log(this.cardList)
+            })
     }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../styles/variables.scss';
 
+* {
+    font-family: $main-font-family;
+}
 </style>
