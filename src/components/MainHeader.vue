cd95ce9b58c4eda52a84973482e5b19c730d3b7a
LuigiDiS <luigi.distefano69@gmail.com>
2022-09-07 16:19:08 +0200

    Bootstra Vue e Font Set

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    Ciao a tutti
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+@import '../styles/main.scss';
+</style>
