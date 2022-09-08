fb037a7af425df8589cb7589dac31b0edc0a23b1
LuigiDiS <luigi.distefano69@gmail.com>
2022-09-08 15:49:17 +0200

    Selected Modified

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="hello container py-3 text-center">
     <label for="genre">Choose a genre:</label>
-    <select name="genre" id="tracks" class="ms-2">
-      <option value="..."></option>
-      <option value="rock">Rock</option>
-      <option value="pop">Pop</option>
+    <select name="genre" id="tracks" class="ms-2" v-model="selected">
+      <option disabled value="">Select a genre</option>
+      <option 
+        v-for="(option,i) in options" 
+        :value="selected"
+        :key="i"
+      >
+          {{ option }}
+      </option>
+      <!-- <option value="pop">Pop</option>
       <option value="jazz">Jazz</option>
-      <option value="metal">Metal</option>
+      <option value="metal">Metal</option> -->
     </select>
   </div>
 </template>
@@ -16,6 +22,17 @@ export default {
   name: 'MainHeader',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      selected: '',
+      options: [
+        'Rock',
+        'Pop',
+        'Jazz',
+        'Metal',
+      ]
+    }
   }
 }
 </script>
