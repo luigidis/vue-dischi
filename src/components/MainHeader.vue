202c3d3b6722074b8b05141988ee9f083947d60b
LuigiDiS <luigi.distefano69@gmail.com>
2022-09-08 17:02:09 +0200

    Bonus Started

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -2,14 +2,22 @@
   <div class="hello container py-3 text-center">
     <label for="genre">Choose a genre:</label>
     <select name="genre" id="tracks" class="ms-2" v-model="selected" @change="$emit('select',selected)">
-      <!-- <option disabled value="">Select a genre</option> -->
       <option v-for="(option,i) in options" :value="option" :key="i">
         {{ option }}
       </option>
     </select>
-
+    <div class="mt-2">
+      <label for="artist">Choose an artist:</label>
+      <select name="artist" id="artist" class="ms-2" v-model="selectedArtist">
+        <option v-for="(artist,i) in artists" :value="artist" :key="i">
+          {{ artist }}
+        </option>
+      </select>
+    </div>
   </div>
 </template>
+      
+
 
 <script>
 export default {
@@ -19,6 +27,19 @@ export default {
   },
   data() {
     return {
+      selectedArtist: '',
+      artists: [
+        '',
+        'Bon Jovi',
+        'Queen',
+        'Sting',
+        'Jazz',
+        'Iron Maiden',
+        'Eric Clapton',
+        'Deep Purple',
+        'Metallica',
+        'Dave Weckl',
+      ],
       selected: '',
       options: [
         '',
